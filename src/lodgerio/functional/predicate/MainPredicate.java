da73e8675786a61f2d1a092cfe1b11c0f1d6afb6
JoJo Padlan <padlanau@yahoo.com.au>
2022-03-22 17:11:19 +1100

    Update MainPredicate.java

--- src/lodgerio/functional/predicate/MainPredicate.java
+++ src/lodgerio/functional/predicate/MainPredicate.java
@@ -81,8 +81,27 @@ public class MainPredicate {
 
 
         /*
-         *  Example #4
+         *  Example #4. This lambda expression will remove all StringBuilders in the list that do not have "e" inside them
          */
+           public static void main(String[] args) {
+        
+              List<StringBuilder> list = new ArrayList<>();
+                StringBuilder d1 = new StringBuilder("Working");
+                StringBuilder d2 = new StringBuilder("Eating");
+                StringBuilder d3 = new StringBuilder("Sleeping");
+
+                list.add(d1);
+                list.add(d2);
+                list.add(d3);
+
+                filter(list,(StringBuilder x )-> x.indexOf("e") !=-1);
+                System.out.println(list);
+
+           }
+
+            private static void filter(List<StringBuilder> list, Predicate<StringBuilder> p) {
+                list.removeIf(p);
+            }
 
 
 
